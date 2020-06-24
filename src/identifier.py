6a02983a1cbee650bf4035705fe7c61faa2c2a34
Sebastian  Gonzalez <sebgo@MacBook-Pro.attlocal.net>
2020-06-24 11:15:18 -0600

    Continued to update identifier script and began sample article that will be used to test the identifier.

--- src/identifier.py
+++ src/identifier.py
@@ -1,2 +1,34 @@
+####################################
+### Neccessary Import Statements ###
+####################################
 import pandas as pd
 import numpy as np
+
+##############################################
+### Write the Class That Identifies Errors ###
+##############################################
+class Error_Identifier:
+	"""
+	Class Purpose 
+	-------------
+
+	Attributes
+	----------
+
+	Methods
+	-------
+
+	Init Parameters
+	---------------
+
+	References
+	----------
+	1.
+	"""
+	def __init__(*args, **kwargs):
+		"""
+		"""
+		#
+
+		return None
+
