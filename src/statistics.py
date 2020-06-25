4c54e46a9ac27d530a68e0be691d693c4cf4bbde
Sebastian  Gonzalez <sebgo@MacBook-Pro.attlocal.net>
2020-06-25 13:29:07 -0600

    Made a new python script for the statistics class.

---
+++ src/statistics.py
@@ -0,0 +1,9 @@
+####################################
+### Neccessary Import Statements ###
+####################################
+import pandas as pd
+import numpy as np
+
+#########################################################
+### Write the Class That Calculates Neccessary Stats. ###
+#########################################################
